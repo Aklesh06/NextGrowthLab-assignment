83c3460548f29964cd4820b8b76e857157750c39
Aklesh <akleshpatel0613@gmail.com>
2024-12-24 13:15:40 +0530

    update vecel.json,build_files.sh

--- Scripts/problem2/build_files.sh
+++ Scripts/problem2/build_files.sh
@@ -1,4 +1,6 @@
-pip install --upgrade pip
-pip install -r requirements.txt
+#!/bin/bash
 
-python manage.py collectstatic 
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput
+python3.11 manage.py migrate --noinput
